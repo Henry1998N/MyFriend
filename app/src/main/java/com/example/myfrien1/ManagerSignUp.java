27f74b8a2afeceeb18c87435bc849ab2ced53f77
Henry1998N <93736837+Henry1998N@users.noreply.github.com>
2022-07-21 18:02:07 +0300

    Update ManagerSignUp.java

--- app/src/main/java/com/example/myfrien1/ManagerSignUp.java
+++ app/src/main/java/com/example/myfrien1/ManagerSignUp.java
@@ -17,7 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ManagerSignUp extends AppCompatActivity {
 
-    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://myfrien1-f5a16-default-rtdb.firebaseio.com/");
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("");
     EditText name,pass,confpass,phone,email;
     Button signup;
     @Override
